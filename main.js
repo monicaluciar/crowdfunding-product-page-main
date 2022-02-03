d6e6d1c52c01d9b71a88761914d445d118130a13
Monica <monicaluciar@gmail.com>
2022-02-03 09:22:06 -0400

    add main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".bar-menu");
+console.log("m");
+let menuV = document.querySelector(".menu-box");
+console.log("menuV");
+// menu.addEventListener("click", () => menuV.classList.toggle("open"));
